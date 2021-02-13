6e8932131e096a59d909e30d6767768846e549a5
sravanin15 <sravaninuthanapati15@gmail.com>
2021-02-13 11:18:30 +0530

    Factorial commit

---
+++ Factorial/src/main/java/sravani/App.java
@@ -0,0 +1,13 @@
+package sravani;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Factorial/src/main/java/sravani/FactorialApp.java
@@ -0,0 +1,17 @@
+package sravani;
+
+import java.util.Scanner;
+
+public class FactorialApp {
+	static Scanner sc=new Scanner(System.in);
+public static void main(String[] args) {
+	int n;
+	int fact=1;
+	n=sc.nextInt();
+	for(int i=1;i<=n;i++)
+	{
+		fact=fact*i;
+	}
+	System.out.println("Factorial of "+n+" is: "+fact);
+}
+}
